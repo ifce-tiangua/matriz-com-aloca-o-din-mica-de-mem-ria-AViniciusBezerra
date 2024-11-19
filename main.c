596762b64b45c813d516037787e9e68c7515aecd
AViniciusBezerra <vinicius.bezerra2564@gmail.com>
2024-11-19 15:36:35 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,8 +2,39 @@
 
 int main(){
 
-    //insira aqui seu código
-    printf("Nao implementado ainda...");
+    int linhas, colunas;
+    int **matriz;
+    
+    scanf("%d %d", &linhas, &colunas);
+    if (linhas <= 0 || colunas <= 0) {
+        printf("[matriz vazia]\n");
+        return 0;
+    }
 
-    return 0;
+    matriz = (int **)malloc(linhas * sizeof(int *));
+    
+    for (int i = 0; i < linhas; i++) {
+        matriz[i] = (int *)malloc(colunas * sizeof(int));
+    }
+    
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+    for (int i = 0; i < linhas; i++) {
+        free(matriz[i]);
+    }
+    free(matriz);
+    matriz = NULL;
+
+    return 0; 
 }
